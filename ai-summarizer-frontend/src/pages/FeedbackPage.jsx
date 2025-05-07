7ece09f0146fdbc2c1bd7f3b08b8706d8fdc91f3
Anishhhh12 <165990614+Anishhhh12@users.noreply.github.com>
2025-05-07 18:22:38 +0530

    Update FeedbackPage.jsx

--- ai-summarizer-frontend/src/pages/FeedbackPage.jsx
+++ ai-summarizer-frontend/src/pages/FeedbackPage.jsx
@@ -16,7 +16,7 @@ export default function FeedbackPage() {
     }
 
     try {
-      const response = await axios.post(import.meta.env.VITE_API_BASE_URL+'/api/feedback', {
+      const response = await axios.post(import.meta.env.VITE_API_BASE_URL+'api/feedback', {
         rating,
         feedback,
       });
